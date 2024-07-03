6dd3c1f24b20d34754ab943ac1027583eeeb6639
kimfield98 <kimfield98@gmail.com>
2024-07-03 23:31:35 +0900

    useMemo test

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -2,10 +2,12 @@ import { deepEquals } from "../../../assignment-2/src/basic/basic";
 
 export function createHooks(callback) {
   let state = [];
-  let index = 0;
-  
+  let stateIndex = 0;
+  let memos = [];
+  let memoIndex = 0;
+
   const useState = (initState) => {
-    const hookIndex = index;
+    const hookIndex = stateIndex;
     const setState = (newState) => {
       if (deepEquals(state[hookIndex], newState)) {
         return;
@@ -13,22 +15,30 @@ export function createHooks(callback) {
       state[hookIndex] = newState;
       callback();
     };
-
     if (state[hookIndex] === undefined) {
       state[hookIndex] = initState;
     }
-
-    index++;
+    stateIndex++;
 
     return [state[hookIndex], setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    const hookIndex = memoIndex;
+    if (memos[hookIndex] === undefined) {
+      memos[hookIndex] = {value: fn(), refs};
+    }
+    if (!deepEquals(memos[hookIndex].refs, refs)) {
+      memos[hookIndex] = {value: fn(), refs};
+    }
+    memoIndex++;
+
+    return memos[hookIndex].value;
   };
 
   const resetContext = () => {
-    index = 0;
+    stateIndex = 0;
+    memoIndex = 0;
   }
 
   return { useState, useMemo, resetContext };
