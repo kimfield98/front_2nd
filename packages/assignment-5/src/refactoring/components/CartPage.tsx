502f33499eac91996223540e4376fd444cc37b95
kimfield98 <kimfield98@gmail.com>
2024-07-18 12:50:48 +0900

    [basic] CartPage 컴포넌트에서 CouponApply, OrderSummary 컨테이너 분리

--- packages/assignment-5/src/refactoring/components/CartPage.tsx
+++ packages/assignment-5/src/refactoring/components/CartPage.tsx
@@ -26,23 +26,17 @@ export const CartPage = ({ products, coupons }: Props) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       <ProductList products={products} cart={cart} addToCart={addToCart} />
-      <CartList
-        cart={cart}
-        updateQuantity={updateQuantity}
-        removeFromCart={removeFromCart}
-      />
-
-      <div className="mt-6 bg-white p-4 rounded shadow">
-        <h2 className="text-2xl font-semibold mb-2">쿠폰 적용</h2>
+      <div>
+        <CartList
+          cart={cart}
+          updateQuantity={updateQuantity}
+          removeFromCart={removeFromCart}
+        />
         <CouponApply
           coupons={coupons}
           selectedCoupon={selectedCoupon}
           applyCoupon={applyCoupon}
         />
-      </div>
-
-      <div className="mt-6 bg-white p-4 rounded shadow">
-        <h2 className="text-2xl font-semibold mb-2">주문 요약</h2>
         <OrderSummary
           totalBeforeDiscount={totalBeforeDiscount}
           totalAfterDiscount={totalAfterDiscount}
