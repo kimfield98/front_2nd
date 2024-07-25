37dd117cb866e23289d57dbbac97137c758bc72a
kimfield98 <kimfield98@gmail.com>
2024-07-26 01:55:38 +0900

    test8

--- packages/assignment-6/vitest.config.ts
+++ packages/assignment-6/vitest.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
-    setupFiles: ["./src/setupTests.ts"],
+    setupFiles: ['./src/setupTests.ts'],
     coverage: {
-      provider: "v8",
-      reporter: ["json"],
-      reportsDirectory: "./coverage",
+      provider: 'v8',
+      reporter: ['text', 'json', 'json-summary', 'html'],
+      reportsDirectory: './coverage',
     },
   },
 });
