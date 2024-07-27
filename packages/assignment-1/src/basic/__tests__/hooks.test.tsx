e18f4b7afb32e3288fa8addada9f31b283a76d21
kimfield98 <kimfield98@gmail.com>
2024-07-27 10:34:37 +0900

    build error 수정 (basic1 only 메서드 제거)

--- packages/assignment-1/src/basic/__tests__/hooks.test.tsx
+++ packages/assignment-1/src/basic/__tests__/hooks.test.tsx
@@ -15,7 +15,7 @@ beforeEach(() => {
 
 describe('다양한 hook을 이용하여 테스트코드를 통과할 수 있도록 해봅시다.', () => {
   describe('useState > ', () => {
-    test.only('setState의 내용을 개선하여, 증가 버튼을 누르면 count의 값이 증가하도록 한다.', () => {
+    test('setState의 내용을 개선하여, 증가 버튼을 누르면 count의 값이 증가하도록 한다.', () => {
       render(<UseStateTest/>);
 
       expect(document.body.innerHTML).toBe('<div><div>count: 1<button>증가</button></div></div>');
