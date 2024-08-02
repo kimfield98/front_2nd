218b3ecd64dbb65a93f5bcff7f8fae2ffa64b723
kimfield98 <kimfield98@gmail.com>
2024-08-02 18:32:50 +0900

    [advanced] EventDisplay props 타입 정리

--- packages/assignment-7/src/components/EventDisplay.tsx
+++ packages/assignment-7/src/components/EventDisplay.tsx
@@ -2,33 +2,12 @@ import { Heading, VStack } from '@chakra-ui/react';
 import ViewSelector from './ViewSelector';
 import WeekView from './WeekView';
 import MonthView from './MonthView';
-import { Event } from '../types';
-
-interface EventDisplayProps {
-  viewState: {
-    view: 'week' | 'month';
-    setView: (view: 'week' | 'month') => void;
-  };
-  currentDateState: { currentDate: Date; setCurrentDate: (date: Date) => void };
-  notificationsState: {
-    notifications: {
-      id: number;
-      message: string;
-    }[];
-    setNotifications: React.Dispatch<
-      React.SetStateAction<
-        {
-          id: number;
-          message: string;
-        }[]
-      >
-    >;
-    notifiedEvents: number[];
-    setNotifiedEvents: React.Dispatch<React.SetStateAction<number[]>>;
-  };
-  filteredEvents: Event[];
-  holidays: Record<string, string>;
-}
+import {
+  CurrentDateState,
+  Event,
+  NotificationsState,
+  ViewState,
+} from '../types';
 
 function EventDisplay({
   viewState,
@@ -36,7 +15,13 @@ function EventDisplay({
   notificationsState,
   filteredEvents,
   holidays,
-}: EventDisplayProps) {
+}: {
+  viewState: ViewState;
+  currentDateState: CurrentDateState;
+  notificationsState: NotificationsState;
+  filteredEvents: Event[];
+  holidays: Record<string, string>;
+}) {
   return (
     <VStack flex={1} spacing={5} align="stretch">
       <Heading>일정 보기</Heading>
