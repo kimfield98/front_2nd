d556bc22a564a1cb71f44f9f66f4e91d89daac2a
kimfield98 <kimfield98@gmail.com>
2024-08-20 21:49:26 +0900

    fix: 이미 호출한 API 다시 호출하지 않도록 캐시 구현

--- packages/assignment-10/src/basic/SearchDialog.tsx
+++ packages/assignment-10/src/basic/SearchDialog.tsx
@@ -32,7 +32,7 @@ import {
 import { useScheduleContext } from './ScheduleContext.tsx';
 import { Lecture } from './types.ts';
 import { parseSchedule } from './utils.ts';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { DAY_LABELS } from './constants.ts';
 
 interface Props {
@@ -82,19 +82,45 @@ const TIME_SLOTS = [
 
 const PAGE_SIZE = 100;
 
-const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
-const fetchLiberalArts = () =>
+type ApiFetchFunction = () => Promise<AxiosResponse<Lecture[]>>;
+type CachedApiFetchFunction = () => Promise<AxiosResponse<Lecture[]>>;
+
+const cache = new WeakMap<
+  ApiFetchFunction,
+  Promise<AxiosResponse<Lecture[]>>
+>();
+
+const createCachedFetch = (
+  fetchFn: ApiFetchFunction
+): CachedApiFetchFunction => {
+  return async () => {
+    if (!cache.has(fetchFn)) {
+      const promise = fetchFn();
+      cache.set(fetchFn, promise);
+      return promise;
+    } else {
+      return cache.get(fetchFn)!;
+    }
+  };
+};
+
+const fetchMajors: ApiFetchFunction = () =>
+  axios.get<Lecture[]>('/schedules-majors.json');
+const fetchLiberalArts: ApiFetchFunction = () =>
   axios.get<Lecture[]>('/schedules-liberal-arts.json');
+
+const cachedFetchMajors = createCachedFetch(fetchMajors);
+const cachedFetchLiberalArts = createCachedFetch(fetchLiberalArts);
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
 const fetchAllLectures = async () =>
   await Promise.all([
-    (console.log('API Call 1', performance.now()), fetchMajors()),
-    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), fetchMajors()),
-    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), fetchMajors()),
-    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 1', performance.now()), cachedFetchMajors()),
+    (console.log('API Call 2', performance.now()), cachedFetchLiberalArts()),
+    (console.log('API Call 3', performance.now()), cachedFetchMajors()),
+    (console.log('API Call 4', performance.now()), cachedFetchLiberalArts()),
+    (console.log('API Call 5', performance.now()), cachedFetchMajors()),
+    (console.log('API Call 6', performance.now()), cachedFetchLiberalArts()),
   ]);
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
